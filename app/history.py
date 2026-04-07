542ce4193e9e70abfb6ef4a3142a50b43bf678f7
Vansh Kakkar <kakkarvansh25@gmail.com>
2026-04-07 13:09:35 +0530

    Hosted the database

--- app/history.py
+++ app/history.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from datetime import datetime
 
-client = MongoClient("mongodb://localhost:27017")
+client = MongoClient("mongodb+srv://vanshkakkar241_db_user:HxyM0T6nxJOwtZuH@clausegaurd.eazlunt.mongodb.net/?appName=ClauseGaurd")
 
 db = client["auth_contract_ai"]
 
